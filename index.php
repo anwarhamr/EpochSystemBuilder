f086c60599475bf380d5aa61af2e9fa1577c7ca6
Jonathan Morrison <jonathan.morrison@gmail.com>
2017-03-24 11:25:09 -0600

    clean up landing page and add gitignore

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <html>
 <head>
   <title>Epoch System</title>
+  <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<br /><a href="example1.php">Example #1: All enabled</a>
-<br /><a href="example2.php">Example #2: Sequential</a>
+<h3><a href="example1.php">Example #1: All enabled</a></h3>
+Because of the high probability of combinations that have no result AND the ability to add a custom message for each of those combinations -- this proof of concept would probably be best used as in distributor / internal sales [training] tool to explain things and redirect a customer to better solution.</p>
+<h3><a href="example2.php">Example #2: Sequential</a></h3>
+Because every combination will always have a successful result and everything else is hidden -- this proof of concept would be better customer facing to avoid frustration/discouragement.</p>
+<h3>Database</a></h3>
+Both proof of concepts use the exact same database.
 </body>
 </html>
